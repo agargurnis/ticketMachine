327663f06c292a03e52132be1f2f5da4b3dcc38e
Arvids Gargurnis <arvids.gargurnis@plymouth.ac.uk>
2017-03-10 16:43:40 +0000

    Initial Commit

---
+++ TicketMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicketMachine
+//
+//  Created by Arvids Gargurnis on 10/03/2017.
+//  Copyright © 2017 Arvids Gargurnis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
